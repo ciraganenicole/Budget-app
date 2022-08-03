1ea4aa33d83c608ef73b1910114c70d0716b8849
ciraganenicole <ciraganenicole@gmail.com>
2022-08-03 12:02:59 +0200

    Fix routes issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'home/index'
-  root 'categories#index'
+  
+  root 'home#index'
   resources :categories, only: %i[index show create new destroy update] do
     resources :transancts, only: %i[index show create new destroy update]
   end
